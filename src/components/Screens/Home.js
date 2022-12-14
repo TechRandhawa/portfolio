158487cb7c2bf69bd490c700c52709a4270db7a3
TechRandhawa <techrandhawa1@gmail.com>
2022-12-14 10:21:35 +0530

    home navigate added

--- src/components/Screens/Home.js
+++ src/components/Screens/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import HomeImage1 from './images/homeImage1'
 import HomeImage2 from './images/homeImage2'
 
 const home = () => {
+  const navigate = useNavigate()
   return (
     <>
       <div className="flex">
@@ -13,14 +15,26 @@ const home = () => {
           <HomeImage2 />
         </div>
         <div className="flex items-center z-10 pl-28">
-          <div className='flex flex-col w-10/12'>
+          <div className="flex flex-col w-10/12">
             <div className="flex flex-col font-bold text-6xl text-white">
               <span>Searching for Creative Web Developer.</span>
-              <spam className="text-xl pt-2">Hi, I'm Satnam Singh a Front-end Web Developer from India</spam>
+              <spam className="text-xl pt-2">
+                Hi, I'm Satnam Singh a Front-end Web Developer from India
+              </spam>
             </div>
             <div className="flex w-full pt-9 space-x-20 pl-11 text-white font-semibold">
-              <button className='bg-gradient-to-br from-white to-black px-8 py-2 rounded-3xl'>Hire me</button>
-              <button className='bg-gradient-to-br from-[#C961DE] to-[#2954A3] px-8 py-2 rounded-3xl'>Download CV</button>
+              <button
+                className="bg-gradient-to-br from-white to-black px-8 py-2 rounded-3xl"
+                onClick={() => navigate('contact')}
+              >
+                Hire me
+              </button>
+              <button
+                className="bg-gradient-to-br from-[#C961DE] to-[#2954A3] px-8 py-2 rounded-3xl"
+                onClick={() => navigate('')}
+              >
+                Download CV
+              </button>
             </div>
           </div>
         </div>
